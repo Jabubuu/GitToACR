8216f9dcadd68c53b88c02dbef331d2b9071f2db
Jabubuu <62589626+Jabubuu@users.noreply.github.com>
2020-11-06 12:26:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,24 +14,12 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://io¨-tech.fi"
+          href="https://io-tech.fi"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn about coding
         </a>
-    
-     <div align="center">
-        <a href="https://www.270towin.com/maps/consensus-2020-electoral-map-forecast">
-        <img src="https://www.270towin.com/map-images/consensus-2020-electoral-map-forecast.png" width="800" />
-        </a>
-        <br/>
-        <small>
-        <img style="vertical-align:middle;" src="https://www.270towin.com/uploads/3rd_party_270_30px.png" alt="" /> 
-        Click the map to create your own at 
-        <a href="https://www.270towin.com/maps/consensus-2020-electoral-map-forecast">270toWin.com</a></small>
-        </div>
-
       </header>   
     </div>
   );
